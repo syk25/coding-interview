4a430db0f73d0b89183ac3dbb0cbe041317b2193
김세연 <129013571+syk25@users.noreply.github.com>
2024-10-17 02:59:57 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,8 +1,6 @@
-my_numbers = [int(input()) for _ in range(10)]
-
 left_overs = set()
 
-for number in my_numbers:
-    left_overs.add(number % 42)
+for _ in range(10):
+    left_overs.add(int(input()) % 42)
 
 print(len(left_overs))
