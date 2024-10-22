17a97d0eded6520b435c4938a690f258c1139234
김세연 <129013571+syk25@users.noreply.github.com>
2024-10-22 23:07:55 +0900

    [Bronze II] Title: 수 정렬하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -3,19 +3,7 @@ from sys import stdin
 input = stdin.readline
 
 
-def selection_sort(my_list):
-    
-    my_size = len(my_list)
-    for i in range(my_size - 1):
-        min_idx = i
-        for j in range(i + 1, my_size):
-            if my_list[min_idx] >= my_list[j]:
-                min_idx = j
-        my_list[min_idx], my_list[i] = my_list[i], my_list[min_idx]
-    return
-
-
 n = int(input())
 my_numbers = [int(input()) for _ in range(n)]
-selection_sort(my_numbers)
+my_numbers.sort()
 print(*my_numbers, sep="\n")
